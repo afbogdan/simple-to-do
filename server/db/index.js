34ed2206f06982f79ea63552faa72cc4e85b7efb
Alexandru-Florin Bogdan <bogdan.alexandru1311@gmail.com>
2020-08-17 18:46:15 +0200

    Configure PostgreSQL database using dotenv

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,17 @@
+// set env variables (contains db connection private info)
+require('dotenv').config();
+
 const { Pool } = require("pg");
-const pool = new Pool();
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: 'todo',
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+});
 
 module.exports = {
     query: (text, params) => pool.query(text, params),
--- server/routes/todo.js
+++ server/routes/todo.js
@@ -3,10 +3,15 @@ const db = require('../db')
 
 const router = new Router()
 
+router.get('/', async (req, res) => {
+    const { rows } = await db.query('SELECT * FROM todos');
+    res.send(rows);
+});
+
 router.get('/:id', async (req, res) => {
     const { id } = req.params
-    const { rows } = await db.query('SELECT * FROM users WHERE id = $1', [id])
-    res.send(rows[0])
+    const { rows } = await db.query('SELECT * FROM todos WHERE id = $1', [id])
+    res.send(rows[0]);
 });
 
 // export our router to be mounted by the parent application
